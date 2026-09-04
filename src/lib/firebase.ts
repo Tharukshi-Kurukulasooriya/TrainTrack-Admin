4c72e56845a78018aed39b723aec1c1f02181ca5
Tharukshi Kurukulasooriya <tharukshikurukulasooriya@gmail.com>
2026-09-04 21:18:51 +0530

    feat: update firebase configurations

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore, type Firestore } from "firebase/firestore";
 import { getStorage, type FirebaseStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABs_GB4LpY-ULdjE5ksMe2a-yXYt_dOO0",
-  authDomain: "the-train-track.firebaseapp.com",
-  projectId: "the-train-track",
-  storageBucket: "the-train-track.firebasestorage.app",
-  messagingSenderId: "1002628867612",
-  appId: "1:1002628867612:web:2b72381d59d2285a6c61d9",
-  measurementId: "G-LNEGEWTLW9",
+  apiKey: "AIzaSyARBH7KYOHriHmRmx45VCiI1QKCc12uN2A",
+  authDomain: "traintrack-f83d3.firebaseapp.com",
+  projectId: "traintrack-f83d3",
+  storageBucket: "traintrack-f83d3.firebasestorage.app",
+  messagingSenderId: "796093443043",
+  appId: "1:796093443043:web:bd3090c48d305d43004ed6",
+  measurementId: "G-FTQ7KJLBHZ"
 };
 
 let app: FirebaseApp | null = null;
@@ -25,3 +25,11 @@ export function getFirebase() {
   }
   return { app, db: db!, storage: storage! };
 }
+
+  // apiKey: "AIzaSyABs_GB4LpY-ULdjE5ksMe2a-yXYt_dOO0",
+  // authDomain: "the-train-track.firebaseapp.com",
+  // projectId: "the-train-track",
+  // storageBucket: "the-train-track.firebasestorage.app",
+  // messagingSenderId: "1002628867612",
+  // appId: "1:1002628867612:web:2b72381d59d2285a6c61d9",
+  // measurementId: "G-LNEGEWTLW9",
